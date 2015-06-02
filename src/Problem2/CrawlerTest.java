1f7b5926e9d2eaafb931324fe0986c75bffeef4f
fgg12 <fgg12@FGG>
2015-06-02 22:52:24 +0300

    .

---
+++ src/Problem2/CrawlerTest.java
@@ -0,0 +1,26 @@
+package Problem2;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+
+
+public class CrawlerTest {
+
+    @Test
+    public void crawlTest() throws MalformedURLException, IOException {
+        String expected = "http://ebusiness.free.bg/cards_bank_cards.html";
+        String output = 
+                WebsiteCrawler
+                .crawl(
+                        new URL("http://ebusiness.free.bg"), 
+                        "Ðåâîëâèðàùà")
+                .toString();
+        
+        assertEquals(expected, output);
+    }
+
+}
---
+++ src/Problem2/WebsiteCrawlerMain.java
@@ -0,0 +1,17 @@
+package Problem2;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class WebsiteCrawlerMain {
+    
+    
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        URL url = WebsiteCrawler.crawl(new URL("http://ebusiness.free.bg"), "Ðåâîëâèðàùà");
+        
+        System.out.println(url);
+    }
+
+}
